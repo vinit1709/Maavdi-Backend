fac020738810079b59bb7ff676a61bc6ef64e100
vinit1709 <vinitkapatel17@gmail.com>
2024-09-03 21:13:01 +0530

    changes

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use("/api/admin", adminRoute)
 
 app.use(errorMiddleware);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 connectDb().then(() =>{
     app.listen(PORT, () => {
         console.log(`server is runing at port: ${PORT}`);
