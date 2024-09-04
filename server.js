509897f0f2c422008f844c9253e016f8ce7dfbda
vinit1709 <vinitkapatel17@gmail.com>
2024-09-04 20:58:53 +0530

    changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const connectDb = require("./db/db");
 const errorMiddleware = require('./middlewares/error-middleware');
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credential: true,
 }
